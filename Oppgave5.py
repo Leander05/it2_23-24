e8693c6c34da15425d6775daf43ed1f119c44aa0
Leander05 <leanderroe@hotmail.com>
2023-09-19 12:49:06 +0200

    oppgave

--- Oppgave5.py
+++ Oppgave5.py
@@ -1,1 +1,13 @@
-#p
+
+
+temp = float(input("skriv inn temperatur: "))
+type = input("skriv F for farenheit og C for celsius: ")
+
+if type == "F":
+    temp = (temp - 32) * 5/9
+    print(f"temperaturen i celsius = {temp:.1f}")
+elif type == "C":
+    temp = temp * (9/5) +32 
+    print(f"temperaturen i farenheit = {temp:.1f}")
+else:
+    print("du skrev ikke C eller F")
